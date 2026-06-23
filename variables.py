fd9057eba5cfd0a75b628486de39af2ff9f843d3
Liam Dietrich <liamthomasdietrich@outlook.com>
2026-06-23 17:25:48 +0100

    Day 1: variables and data types

---
+++ variables.py
@@ -0,0 +1,16 @@
+name = "Liam"
+age = 28
+job_title = "AI&Data Consultant"
+city = "Belfast"
+salary = 35000.50
+employed = True
+
+
+print (name, age, job_title, city, salary, employed)
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(employed))
+
+print(name + " " + city)
