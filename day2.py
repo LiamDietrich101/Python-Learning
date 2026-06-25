59cc313e9c62da1a9f6c7ad12b7ea022828ed556
Liam Dietrich <liamthomasdietrich@outlook.com>
2026-06-25 15:49:39 +0100

    Day 2: string methods

---
+++ day2.py
@@ -0,0 +1,19 @@
+#name = "liam dietrich"
+#print(name.upper())
+#print(name.lower())
+#print(name.title())
+
+#name = "   liam dietrich   "
+#print(name.strip())
+#print(name.replace("liam", "john"))
+#print(len(name))
+
+
+#Create a variable called full_name storing your name in all lowercase with extra spaces at the beginning and end
+#Print it stripped of spaces and in title case — on one line
+#Print the number of characters in your name without the extra spaces
+
+full_name = "   liam dietrich   "
+new_name = (full_name.strip())
+print (new_name.title())
+print(len(new_name))
