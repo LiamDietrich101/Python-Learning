d68180a4e60be010fd95117ef0e142469112d2b9
Liam Dietrich <liamthomasdietrich@outlook.com>
2026-06-30 12:13:40 +0100

    Day 5: for loops, range, multiplication table

---
+++ Day5.py
@@ -0,0 +1,22 @@
+#print ("Hello world " * 5)
+
+#for i in range(5):
+#    print("Hello world")
+
+#for i in range(5):
+#    print(i)
+
+#for i in range(1,6):
+#    print(f"This is loop number {i}")
+
+#for i in range(0, 10, 2):
+#    print(i)
+
+print("pick a number")
+number = input()
+number = int(number)
+print (f"you picked {number}")
+for i in range(1,11):
+    print(f"{number} * {i} = {number*i}")
+
+
