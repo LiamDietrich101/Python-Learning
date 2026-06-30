0ffa142ffe6e7098f0f3493da5ca2f919c42a32a
Liam Dietrich <liamthomasdietrich@outlook.com>
2026-06-30 17:01:17 +0100

    Day 6: while loops, password validation

---
+++ Day6.py
@@ -0,0 +1,16 @@
+#example
+'''
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+'''
+
+
+password = input("Please type the password ")
+while password != "python123":
+    password = input("Please type the password ")
+print ('That is correct')
+
+
+
