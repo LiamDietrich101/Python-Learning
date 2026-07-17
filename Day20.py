6820d5d6226f00d7c9cac62c6da0f8d6d3849e40
Liam Dietrich <liamthomasdietrich@outlook.com>
2026-07-17 22:07:22 +0100

    Day 20 - escape characters, sep and end in print

---
+++ Day20.py
@@ -0,0 +1,24 @@
+print("Hello\nWorld")
+print("Name:\tLiam")
+print("This is a backslash: \\")
+print("He said \"hello\"")
+print("He said \'hello\'")
+
+
+
+print("a", "b", "c")        # a b c
+print("a", "b", "c", sep="-")  # a-b-c
+print("a", "b", "c", sep="")   # abc
+
+
+
+print("Hello", end="")
+print("World")
+# HelloWorld — no line break between them
+
+print("Hello", end=" ")
+print("World")
+# Hello World — space instead of new line
+
+
+
